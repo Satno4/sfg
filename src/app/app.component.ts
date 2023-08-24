e409bc477f6fbc8836331ce2636e2504504be8a2
Satno4 <besariongagelidze@gmail.com>
2023-08-24 18:49:28 +0400

    hosting test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3-fetch';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sfg';
+  title = 'sfy';
+  constructor() {
+    d3.csv(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSiWiD1kxXugepZ6iJ-QAI2csieusNCbBTcDZhNeOUdMlXtufCu3nmXU9rQTZNIOLnslFsL1aafRnxj/pub?gid=888593843&single=true&output=csv'
+    ).then(console.log);
+  }
 }
